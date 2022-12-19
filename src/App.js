2a731afc9db420a6d102e017c8a5d0e982432832
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-12-19 17:32:09 +0200

    Changes in two other files(App.js, index.js) regarding Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Quotes from "./pages/Quotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <Quotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new_quote">
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
