e4c4218d501ec5c3ff7bc7a561d3e13bd437218f
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-12-20 11:20:20 +0200

    Adding dummy data and logic for adding quotes.

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,5 +1,11 @@
+import QuoteList from "../components/quotes/QuoteList";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Alex", text: "Learning React is great!" },
+  { id: "q2", author: "Mike", text: "Learning React is hard.." },
+  { id: "q3", author: "Maria", text: "Learning React is not hard at all!" },
+];
 const Quotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default Quotes;
