9239deba0dfbc69d00230642b193f45b38669742
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2022-12-20 11:37:25 +0200

    Adding quote details page information.

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,22 @@
 import { useParams, Route } from "react-router-dom";
 import { Fragment } from "react";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Alex", text: "Learning React is great!" },
+  { id: "q2", author: "Mike", text: "Learning React is hard.." },
+  { id: "q3", author: "Maria", text: "Learning React is not hard at all!" },
+];
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((item) => item.id === params.quoteId);
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
   return (
     <Fragment>
       <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
