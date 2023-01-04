6f9eb73da10d6cd1d31646cf8f8457faca5c1935
Aleksander Cvetkov <aleksandar.k.tsvetkov@gmail.com>
2023-01-04 17:56:47 +0200

    Optimizing the code - adding lazy loading for New Quote, Quote Detail and Error pages.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
+import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Quotes from "./pages/Quotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
-import ErrorPage from "./pages/ErrorPage";
+import { Suspense } from "react";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const ErrorPage = React.lazy(() => import("./pages/ErrorPage"));
 function App() {
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <Quotes />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="/new_quote">
-            <NewQuote />
-          </Route>
-          <Route path="*">
-            <ErrorPage />
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/quotes" exact>
+              <Quotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+              <QuoteDetail />
+            </Route>
+            <Route path="/new_quote">
+              <NewQuote />
+            </Route>
+            <Route path="*">
+              <ErrorPage />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </div>
   );
